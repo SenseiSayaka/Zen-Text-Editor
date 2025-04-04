3037f83658b2301c64fd4e46268d77ac85d05a43
SenseiSayaka <marat3112965@mail.ru>
2025-04-04 18:03:45 +0300

    improve previous keys arrows, expanded with more keys (HM, PU,PD, END

--- zen.c
+++ zen.c
@@ -32,6 +32,19 @@ struct editorConfig
 
 };
 
+enum editorKey
+{
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
+};
+
 struct editorConfig E;
 
 
@@ -103,7 +116,7 @@ void enableRawMode()
         die("tcsetattr");
 }
 
-char editorReadKey()
+int editorReadKey()
 {
     int nread;
     char c;
@@ -112,7 +125,60 @@ char editorReadKey()
         if (nread == -1 && errno !=EAGAIN)
             die("read");
     }
-    return c;
+    
+    if(c == '\x1b')
+    {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+        if (seq[0] == '[')
+        {
+            if(seq[1] >= '0' && seq[1] <= '9')
+            {
+                if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if(seq[2] == '~')
+                {
+                    switch(seq[1])
+                    {
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;          
+                    }
+                }
+            }
+            else
+            {
+            switch(seq[1])
+            {
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN; 
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
+                }
+            }
+        } 
+        else if(seq[0] == 'O')
+        {
+            switch(seq[1])
+            {
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
+            }
+        }
+        return '\x1b';
+
+    }   else
+        {
+            return c;
+        }        
 }
 
 
@@ -133,9 +199,7 @@ int getWindowSize(int *rows, int *cols)
 }
 
 
-
 // *** append buffer *** //
-
 
 struct abuf
 {
@@ -228,37 +292,42 @@ void editorRefreshScreen()
     abFree(&ab);
 }
 
-
-
-
 // *** input *** //
 
-
-
-void editorMoveCursor(char key)
+void editorMoveCursor(int key)
 {
     switch(key)
     {
-        case 'a':
-            E.cx--;
+        case ARROW_LEFT:
+            if(E.cx != 0)
+            {
+                E.cx--;
+            }
             break;
-        case 'd':
-            E.cx++;
+        case ARROW_RIGHT:
+            if(E.cx != E.screencols - 1)
+            {
+                E.cx++;
+            } 
             break;
-        case 'w':
-            E.cy--;
+        case ARROW_UP:
+            if(E.cy != 0)
+            {
+                E.cy--;
+            }
             break;
-        case 's':
-            E.cy++;
+        case ARROW_DOWN:
+            if(E.cy != E.screenrows - 1)
+            {
+                E.cy++;
+            }
             break;
     }
 }
 
-
-
 void editorProcessKeypress()
 {
-    char c = editorReadKey();
+    int c = editorReadKey();
 
     switch(c)
     {
@@ -270,18 +339,33 @@ void editorProcessKeypress()
     
             exit(0);
             break;
-       
-        case 'w':
-        case 's':
-        case 'a':
-        case 'd':
+        
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols - 1;
+            break;
+    
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screenrows;
+                while(times--)
+                    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+            break;
+
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
+
     }
 
 }
-
-
 
 // *** init *** //
 
