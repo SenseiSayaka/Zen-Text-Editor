291be3e8817e83fce854bb00b09d42e2de6b932c
SenseiSayaka <marat3112965@mail.ru>
2025-04-04 12:54:36 +0300

    updated with append buffer, moving cursor with keys, editor interface

--- zen.c
+++ zen.c
@@ -1,30 +1,58 @@
+// *** includes *** //
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+#include <string.h>
 
-//original terminal attributes in orig_termios
-struct termios orig_termios;
+// *** defines *** //
 
+#define ZEN_VERSION "1.0"
+#define CTRL_KEY(k) ((k) & 0x1f)
+#define ABUF_INIT {NULL, 0}
+
+
+// *** data *** //
+
+
+// editor state
+struct editorConfig
+{
+    int cx;  // cursor's x pos
+    int cy;  // cursor's y pos
+                    
+    int screenrows;  // row
+    int screencols;  // columns
+   
+    //original terminal attributes in orig_termios
+    struct termios orig_termios;
+
+};
+
+struct editorConfig E;
+
+
+
+// *** terminal *** //
 
 
 // error handling
 void die(const char *s)
 {   
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    
     perror(s);
     exit(1);
 }
 
-
-
-
-
 // yup, disable
 void disableRawMode()
 {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
@@ -42,13 +70,13 @@ void enableRawMode()
 {
     // with tcgetattr we get terminal attributes
    
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
         die("tcgetattr");
    
     // atexit from stdlib, we use it to register our disable mode function to be called automatically    
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     
 
     // c_lflag field for "local flags", which describe behavior of terminal i/o on user level, signals, echo,
@@ -75,28 +103,206 @@ void enableRawMode()
         die("tcsetattr");
 }
 
+char editorReadKey()
+{
+    int nread;
+    char c;
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if (nread == -1 && errno !=EAGAIN)
+            die("read");
+    }
+    return c;
+}
+
+
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+    
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        return -1;
+    }
+    else 
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+
+
+// *** append buffer *** //
+
+
+struct abuf
+{
+    char *b;
+    
+    int len;
+};
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+    char *new = realloc(ab->b, ab->len + len);
+    
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+
+void abFree(struct abuf *ab)
+{
+    free(ab->b);
+}
+
+
+// *** output *** //
+
+void editorDrawRows(struct abuf *ab)
+{
+    int y;
+    for (y = 0; y < E.screenrows; y++)
+    {
+        if(y == E.screenrows / 3)
+        {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                    "Zen Version -- version %s", ZEN_VERSION);
+            if(welcomelen > E.screencols) welcomelen = E.screencols;
+            
+            int padding = (E.screencols - welcomelen) / 2;
+            if(padding)
+            {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while(padding--)
+                abAppend(ab, " ", 1);
+
+            abAppend(ab, welcome, welcomelen);
+        }
+        else
+        {
+
+            abAppend(ab, "~", 1);
+        }
+
+        // clear line when redraw instead clear whole terminal
+
+        abAppend(ab, "\x1b[K", 3);
+
+        if (y < E.screenrows - 1)
+        {
+            
+            abAppend(ab, "\r\n", 2);
+        }
+    }
+}
+
+
+// in this section we uses escape sequences to clear the screen, move the cursor, formatting print , etc.
+void editorRefreshScreen()
+{
+    struct abuf ab = ABUF_INIT;
+
+    abAppend(&ab, "\x1b[?25l", 6);
+
+
+    //move cursor
+    abAppend(&ab, "\x1b[H", 3);
+
+    editorDrawRows(&ab);
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
+    abAppend(&ab, "\x1b[?25h", 6);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
+}
+
+
+
+
+// *** input *** //
+
+
+
+void editorMoveCursor(char key)
+{
+    switch(key)
+    {
+        case 'a':
+            E.cx--;
+            break;
+        case 'd':
+            E.cx++;
+            break;
+        case 'w':
+            E.cy--;
+            break;
+        case 's':
+            E.cy++;
+            break;
+    }
+}
+
+
+
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+
+    switch(c)
+    {
+        case CTRL_KEY('q'):
+
+    
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+    
+            exit(0);
+            break;
+       
+        case 'w':
+        case 's':
+        case 'a':
+        case 'd':
+            editorMoveCursor(c);
+            break;
+    }
+
+}
+
+
+
+// *** init *** //
+
+void initEditor()
+{
+    E.cy = 0;
+    E.cx = 0;
+
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+        die("getWindowSize");
+}
 
 int main()
 {
      enableRawMode();
+     initEditor();
 
      while(1)
      {
-         char c = '\0';
-         read(STDIN_FILENO, &c, 1);
-         if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-             die("read");
-
-         // iscnrtl from ctype.h and it check whether a character is a control character 
-         if(iscntrl(c))
-         {
-             printf("%d\r\n", c);
-         }
-         else 
-         {
-             printf("%d ('%c')\r\n", c, c);
-         }
-         if (c == 'q') break;
+         editorRefreshScreen();
+         editorProcessKeypress();
      }
       
      return 0;
